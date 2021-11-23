ef5976aca16819fd483409b3ad42118735135ef2
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-22 20:12:44 -0500

    Add comments

--- qiskit_alt/convert_types.py
+++ qiskit_alt/convert_types.py
@@ -44,9 +44,9 @@ def Geometry(qiskit_geometry):
     return Main.qiskt_geometry_to_Geometry(qiskit_geometry)
 
 def qubit_hamiltonian(geometry, basis):
-    jlgeometry = Geometry(geometry)
-    pauli_op = Main.qubit_hamiltonian(jlgeometry, basis)
-    spop_jl = QiskitQuantumInfo.SparsePauliOp(pauli_op)
-    spop = jlSparsePauliOp(spop_jl)
+    jlgeometry = Geometry(geometry) # Convert Python geometry spec to ElectronicStructure
+    pauli_op = Main.qubit_hamiltonian(jlgeometry, basis) # Compute Pauli operator as QuantumOps.PauliSum
+    spop_jl = QiskitQuantumInfo.SparsePauliOp(pauli_op) # Convert to QiskitQuantumInfo.SparsePauliOp
+    spop = jlSparsePauliOp(spop_jl)  # Convert to qisit.quantum_info.SparsePauliOp
     return spop
 
