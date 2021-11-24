c352a4488ee4bdb575612c82e30c334654211956
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-24 18:23:15 -0500

    modified:   bench/pauli_from_list_alt.py 	modified:   demos/qiskit_alt_demo.ipynb

--- bench/pauli_from_list_alt.py
+++ bench/pauli_from_list_alt.py
@@ -15,6 +15,7 @@ qkalt_times = []
 for k in (10, 100):
     for n in (10, 100, 1000, 5000, 10_000, 100_000):
         label = rand_label(k, n)
+        PauliSum_to_SparsePauliOp(QuantumOps.PauliSum(label))
         number = 20
         t = timeit(lambda: PauliSum_to_SparsePauliOp(QuantumOps.PauliSum(label)), number=number)
         t /= number
