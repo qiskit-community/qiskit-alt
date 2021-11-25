5113a5da89bd5147cb7283b23be43eed725f46f5
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-25 14:33:37 -0500

    Automate startup a bit more

--- qiskit_alt/activate_julia.py
+++ qiskit_alt/activate_julia.py
@@ -1,19 +1,24 @@
 import julia
 
 from julia.api import LibJulia
-# julia_path = "/home/lapeyre/.local/julias/julia-1.5/bin/julia"
+julia_path = ""
 # julia_path = "/home/lapeyre/.local/julias/julia-1.6/bin/julia"
 # julia_path = "/home/lapeyre/.local/julias/julia-1.8.0-DEV.979/bin/julia"
 julia_path = "/home/lapeyre/.local/julias/julia-1.7.0-beta4/bin/julia"
 
-from os.path import dirname
-toplevel = dirname(dirname(__file__))
+import os
+toplevel = os.path.dirname(os.path.dirname(__file__))
 
-from julia import Julia
-jl = Julia(sysimage= toplevel + "/sys_quantum.so")
+# TODO: support mac and win here
+sys_image_path = toplevel + "/sys_quantum.so"
+if os.path.exists(sys_image_path):
+    from julia import Julia
+    jl = Julia(sysimage= sys_image_path)
 
-api = LibJulia.load(julia=julia_path)
-api.init_julia()
+# If the binary does not exist, the standard search path will be used
+if os.path.exists(julia_path):
+    api = LibJulia.load(julia=julia_path)
+    api.init_julia()
 
 from julia import Main
 from julia import Base
