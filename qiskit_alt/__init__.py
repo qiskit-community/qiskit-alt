00cc1b76136d1757d264a3f801008cbaf3b87ecb
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-12-10 14:10:25 -0500

    Add __version__

--- qiskit_alt/__init__.py
+++ qiskit_alt/__init__.py
@@ -3,3 +3,5 @@ from julia import  Main, Base, Pkg
 from julia import QuantumOps, QiskitQuantumInfo, ElectronicStructure
 from .pauli_operators import jlPauli, jlSparsePauliOp, PauliSum_to_SparsePauliOp
 from .electronic_structure import Geometry, jordan_wigner, fermionic_hamiltonian
+
+from ._version import __version__
