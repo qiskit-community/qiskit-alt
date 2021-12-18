074ba2539c261e5c886a1caabceccd796742b8cf
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-12-18 11:16:19 -0500

    Build system image with correct suffix for platform

--- sys_image/compile_julia_project.jl
+++ sys_image/compile_julia_project.jl
@@ -1,6 +1,9 @@
 using PackageCompiler
+using Libdl: Libdl
 
 packages = [:PyCall, :QuantumOps, :ElectronicStructure, :QiskitQuantumInfo]
 
-create_sysimage(packages; sysimage_path="sys_julia_project.so",
+sysimage_path = "sys_julia_project." * Libdl.dlext
+
+create_sysimage(packages; sysimage_path=sysimage_path,
                 precompile_execution_file="compile_exercise_script.jl")
