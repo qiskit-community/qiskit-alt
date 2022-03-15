332056e9b2501e78b43c102ba9bad02be5a715af
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-03-14 22:05:54 -0400

    Store module version in a single place

--- src/qiskit_alt/__init__.py
+++ src/qiskit_alt/__init__.py
@@ -1,8 +1,8 @@
+__version__ = "0.1.5"
+
 from ._julia_project import project
 
 # QuantumOps = project.simple_import("QuantumOps")
 # from julia import QuantumOps, QiskitQuantumInfo, ElectronicStructure
 # from .pauli_operators import jlPauli, jlSparsePauliOp, PauliSum_to_SparsePauliOp
 # from .electronic_structure import Geometry, jordan_wigner, fermionic_hamiltonian
-
-from ._version import __version__
