67b6e7842f7ec2a7fd6fa03bd5cf883e4bb53cd2
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-04 13:15:01 -0400

    Deactivate venvs in test script after they are used
    If we test virtual environments (venv) and then conda virtual
    environments, without deactivating the former, an error is
    raised. See also #23.
    
    Fix bug introduced in last commit
    
    I deactivated the venvs too early.
    
    Fix another bug in script run_init_tests.sh
    
    The past three commits have been squashing bugs in
    run_init_tests.sh

--- run_init_tests.sh
+++ run_init_tests.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/sh
 
-echo source ./venv1/bin/activate && python init_test.py
+# Must deactivate to avoid complaint about mixed virtual environments
+echo "source ./venv1/bin/activate && python init_test.py"
 source ./venv1/bin/activate && python init_test.py
 echo
+deactivate
 
-echo source ./venv1/bin/activate && python init_test.py
+echo "source ./venv1/bin/activate && python init_test.py"
 source ./venv_static/bin/activate && python init_test.py
 echo
+deactivate
 
 # Work around poor design
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
-echo conda activate qiskit_alt_env && python init_test.py
+echo "conda activate qiskit_alt_env && python init_test.py"
 conda activate qiskit_alt_env && python init_test.py
