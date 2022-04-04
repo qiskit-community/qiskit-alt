9b2133b7b3b4a1f8f66b59f8fe42007ef8621076
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-04 10:34:46 -0400

    Work around conda bug

--- run_init_tests.sh
+++ run_init_tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
 source ./venv1/bin/activate && python init_test.py
-conda activate qiskit_alt_env && python init_test.py
 source ./venv_static/bin/activate && python init_test.py
+
+# Work around poor design
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate qiskit_alt_env && python init_test.py
