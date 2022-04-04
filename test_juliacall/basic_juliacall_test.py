192f5b94e6c4e3b4aa7097028e30b6c869f533ce
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-04 18:56:48 -0400

    Organize and comment docker file

--- test_juliacall/basic_juliacall_test.py
+++ test_juliacall/basic_juliacall_test.py
@@ -3,14 +3,11 @@ import qiskit_alt
 project = qiskit_alt.project
 project.ensure_init(calljulia="juliacall")
 
-
 def test_always_passes():
     assert True
 
-
 def test_interface_lib():
     assert qiskit_alt.project.julia.__name__ == 'juliacall'
-
 
 def test_Main():
     Main = qiskit_alt.project.julia.Main
