d192efbeac4381099d8c34fe196505e4a0752b77
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-04 11:38:06 -0400

    Improve output in test script

--- run_init_tests.sh
+++ run_init_tests.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/sh
 
+echo source ./venv1/bin/activate && python init_test.py
 source ./venv1/bin/activate && python init_test.py
+echo
+
+echo source ./venv1/bin/activate && python init_test.py
 source ./venv_static/bin/activate && python init_test.py
+echo
 
 # Work around poor design
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
+echo conda activate qiskit_alt_env && python init_test.py
 conda activate qiskit_alt_env && python init_test.py
