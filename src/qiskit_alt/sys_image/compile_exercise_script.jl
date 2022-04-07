8a8ffad21c04db63d3d949deb9955ff76e7c40e1
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-04-07 18:02:18 -0400

    Run QuantumCircuits tests when compiling

--- src/qiskit_alt/sys_image/compile_exercise_script.jl
+++ src/qiskit_alt/sys_image/compile_exercise_script.jl
@@ -1,7 +1,13 @@
 import QuantumOps
 import ElectronicStructure
 import QiskitQuantumInfo
+import QuantumCircuits
 
+try
+    include(joinpath(pkgdir(QuantumCircuits), "test", "runtests.jl"))
+catch e
+    nothing
+end
 include(joinpath(pkgdir(QuantumOps), "test", "runtests.jl"))
 include(joinpath(pkgdir(ElectronicStructure), "test", "runtests.jl"))
 include(joinpath(pkgdir(QiskitQuantumInfo), "test", "runtests.jl"))
